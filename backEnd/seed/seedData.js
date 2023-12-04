240a2d6027120b77c3663d316141069ba40b26a0
TechALynch <Anthony.Lynch001@outlook.com>
2023-12-04 04:02:49 -0500

    Skill1 Complete

--- backEnd/seed/seedData.js
+++ backEnd/seed/seedData.js
@@ -3,6 +3,7 @@ const { Category, Exercise, Skill, User, Workout} = require('../models/index');
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 console.log('DB On');
 
+//Function used to Create or skip exercise that are in each workout group array
 const findOrCreateExercise = async (exerciseData) => {
   const existingExercise = await Exercise.findOne({ name: exerciseData.name });
   if (existingExercise) {
@@ -169,17 +170,17 @@ const main = async () => {
   `,
   description:
   `
-  Description
+  **Description**
   The Pull-Up is a fundamental calisthenic exercise that serves as a prerequisite for mastering more advanced bar skills. The primary progression goal is to establish basic upper body strength, laying the foundation for achieving a Muscle Up.
 
-  Execution:
+  **Execution**:
   1. Begin in a dead hang position.
   2. Initiate the movement by depressing and retracting the scapula.
   3. Engage your biceps and back muscles to pull yourself up, aiming for your chin to surpass the bar or ideally, your chest to touch the bar.
   4. Descend slowly and with control back to the dead hang position.
   5. It's crucial to perform the full range of motion for maximum effectiveness.
   
-  Variations:
+  **Variations**:
   The Chin-Up, done with an underhand grip, differs from the overhand grip of the Pull-Up. The underhand grip causes your elbows to naturally bend more inward, targeting the biceps to a greater extent.
   The Pull-Up can also be performed on rings instead of bars.`,
   categories: [bodyCategoriesArms._id, bodyCategoriesBack._id]
@@ -196,17 +197,17 @@ const main = async () => {
     `,
   description:
     `
-    Description
+    **Description**
     The Body Row is an excellent beginner exercise that targets the muscles of the upper back and arms. It is an effective way to build strength in the pulling muscles and is a great precursor to more challenging pulling exercises.
 
-    Execution:
+    **Execution**:
     1. Set up a bar at waist height.
     2. Lie on your back under the bar.
     3. Grab the bar with an overhand grip.
     4. Keep your body straight and pull your chest towards the bar.
     5. Lower your body back down with control.
     
-    Variations:
+    **Variations**:
     Adjust the difficulty by changing the height of the bar. A higher bar makes it easier, while a lower bar increases the challenge.
     `,
     categories: [bodyCategoriesArms._id, bodyCategoriesBack._id]
@@ -225,17 +226,17 @@ const main = async () => {
     `,
   description:
     `
-    Description
+    **Description**
     The Dip is a foundational calisthenic exercise that targets the muscles of the chest, triceps, and shoulders. It is an excellent way to build upper body strength and is often used in various fitness routines.
 
-    Execution:
+    **Execution**:
     1. Use parallel bars or sturdy surfaces.
     2. Grip the bars with hands shoulder-width apart.
     3. Lower your body by bending your elbows.
     4. Keep your body upright and avoid leaning too far forward.
     5. Push through the bars to return to the starting position.
     
-    Variations:
+    **Variations**:
     To increase difficulty, add weight with a dip belt or perform deep dips for an extended range of motion.
     `,
     categories: [bodyCategoriesArms._id, bodyCategoriesChest._id]
@@ -253,17 +254,17 @@ const main = async () => {
     `,
   description:
     `
-    Description
+    **Description**
     The Push-Up is a classic bodyweight exercise that targets the chest, shoulders, triceps, and core muscles. It is versatile and can be adapted for all fitness levels.
 
-    Execution:
+    **Execution**:
     1. Start in a plank position with hands placed slightly wider than shoulder-width.
     2. Keep a straight line from head to heels.
     3. Lower your chest towards the ground by bending your elbows.
     4. Ensure a full range of motion by bringing your chest close to the ground.
     5. Push through your palms to return to the starting position.
     
-    Variations:
+    **Variations**:
     Modify intensity by performing incline or decline push-ups. You can also experiment with hand placement for different muscle emphasis.
     `,
     categories:[bodyCategoriesArms._id, bodyCategoriesChest._id]
@@ -319,7 +320,7 @@ const ExerciseGroup2 = [
     categories: [bodyCategoriesArms._id, bodyCategoriesBack._id]
   },
   {
-    image: './UNIT5/week1/mon/GravityGains/backEnd/seed/Images/exerciseImages/PullUp3x7.png',
+    image: './UNIT5/week1/mon/GravityGains/backEnd/seed/Images/exerciseImages/PullUp.png',
     name: 'Pull-Up',
     lvl: 'Beginner',
     video: 'https://www.youtube.com/watch?v=GBqAZP6jquc',
@@ -539,6 +540,260 @@ const MuscleUpStartJumping = await Workout.create({
 });
 console.log(`Workout "${MuscleUpStartJumping.name}" created with exercises:`, G3exerciseIds);
 
+const ExerciseGroup4 = [
+  {
+    image: './GravityGains/backEnd/seed/Images/exerciseImages/KippingMuscleUp.png',
+    name: 'Kipping Muscle Up',
+    lvl: 'Advanced',
+    video: 'https://youtu.be/PMEhv0-WC34?si=6fC4_nb8dDKLefl7',
+    techniqueSummary: `
+      1. Swing forward
+      2. Use momentum
+      3. Rotate wrists
+      4. Shoulders over the bar
+      5. Elbows pullback
+    `,
+    description: `
+      **Description:**
+      The Kipping Muscle Up is an advanced calisthenic exercise that leverages controlled swinging and momentum to perform a dynamic transition over the bar. This technique-intensive movement engages various muscle groups and demands precise coordination for a fluid execution.
+  
+      **Execution:**
+      1. Initiate the movement by swinging forward to build up momentum.
+      2. As you swing back, use the accumulated momentum as extra assistance to pull yourself as high as possible.
+      3. Shortly before reaching the highest point, pull yourself towards the bar, focusing on pulling your elbows backward.
+      4. Actively rotate your wrists during the pull and throw your shoulders over the bar.
+      5. Once your chest is over the bar, execute a dip to extend your arms fully.
+      6. For the second repetition, use the momentum generated from the descent, similar to the initial swing forward.
+  
+      **Tips:**
+      - Master the timing of the swing and pull for efficient use of momentum.
+      - Practice controlled wrist rotation during the entire movement.
+      - Focus on maintaining a smooth and controlled descent to set up for subsequent repetitions.
+    `,
+    categories: [bodyCategoriesArms._id, bodyCategoriesBack._id, bodyCategoriesChest._id]
+  },
+  {
+    image: './UNIT5/week1/mon/GravityGains/backEnd/seed/Images/exerciseImages/HighPullUp.png',
+    name: 'High Pull Up',
+    lvl: 'Advanced',
+    video: 'https://www.youtube.com/watch?v=vk3OuWhoS5Y',
+    techniqueSummary: `
+      1. Elbows move behind back
+      2. Hollow Body
+      3. Pull behind bar instead under bar
+      4. Tighten glutes and core
+      5. Small swing prior pull
+    `,
+    description: `
+      **Description:**
+      The High Pull Up is an advanced calisthenic exercise designed to target the muscles of the upper back and arms with an emphasis on achieving a higher pull. This variation adds complexity and intensity to the traditional Pull-Up, challenging your strength and control.
+  
+      **Execution:**
+      1. Start from a dead hang position with a slightly wider than shoulder-width grip.
+      2. Initiate the pull by moving your elbows behind your back, engaging the upper back muscles.
+      3. Maintain a hollow body position throughout the movement by tightening your glutes and core.
+      4. Instead of pulling directly under the bar, aim to pull your chest or upper body behind the bar.
+      5. Add a small swing prior to the pull, introducing an element of dynamic control to the exercise.
+  
+      **Variations:**
+      As an advanced variation, mastering basic Pull-Up mechanics is crucial before attempting the High Pull Up. Focus on performing each repetition with precision and control, ensuring a full range of motion for optimal results.
+    `,
+    categories: [bodyCategoriesArms._id, bodyCategoriesBack._id]
+  },
+  {
+    image: './GravityGains/backEnd/seed/Images/exerciseImages/NegativeMuscleUp.png',
+    name: 'Negative Muscle Up',
+    lvl: 'Intermediate',
+    video: 'https://youtu.be/70y7r0vBddM?si=MmIrEZ5rXYcnpFb_',
+    techniqueSummary: `
+      1. Slow
+    `,
+    description: `
+      **Description:**
+      The Negative Muscle Up is an intermediate calisthenic exercise that focuses on the eccentric (lowering) phase of the Muscle Up. By performing a controlled descent from the top position to a dead hang, this exercise enhances strength and stability in preparation for the full Muscle Up.
+  
+      **Execution:**
+      1. Start in the support position, using a lower bar, jumping up, or transitioning from a Pullover exercise to get on top.
+      2. Slowly lower yourself with a smooth transition until you reach a dead hang position.
+      3. Pay close attention to executing the exercise as slowly as possible to maximize the eccentric loading on the muscles.
+      4. Keep your elbows in throughout the movement, maintaining proper form for the upward phase of the Muscle Up.
+      
+      **Tips:**
+      - Control the descent to challenge your muscles and build strength.
+      - Focus on a smooth transition during the lowering phase to mimic the Muscle Up movement.
+      - Keeping your elbows in is crucial for reinforcing the correct muscle engagement.
+  
+      *Note: Timestamp "3:06" may indicate a specific point in the video for additional guidance.*
+    `,
+    categories: [bodyCategoriesArms._id, bodyCategoriesBack._id, bodyCategoriesChest._id]
+  },  
+  {
+    image: './GravityGains/backEnd/seed/Images/exerciseImages/StraightBarDip.png',
+    name: 'Straight Bar Dip',
+    lvl: 'Beginner',
+    video: 'https://youtu.be/70y7r0vBddM?si=25ThDoaerUJ7jhwr',
+    techniqueSummary: `
+      1. Lower to about 90°
+      2. Keep scapula retracted on the way down
+      3. Keep elbows in
+    `,
+    description: `
+      **Description:**
+      The Straight Bar Dip is a beginner-level exercise that targets the muscles of the upper body, particularly the chest and triceps. It is an effective bodyweight movement for building strength and endurance in the arms and shoulders.
+  
+      **Execution:**
+      Start in the support position on a straight bar. Slowly lower yourself until your arms are at least in a 90° angle. On the way down, keep your scapula retracted, your elbows in, and your core tight. Push yourself up to the starting position.
+  
+      **Learn more:**
+      [Watch the video tutorial](https://youtu.be/70y7r0vBddM?si=25ThDoaerUJ7jhwr)
+    `,
+    categories: [bodyCategoriesArms._id, bodyCategoriesChest._id]
+  },
+];
+
+const G4exerciseIds = [];
+  
+for (const ExerciseGroup4Data of ExerciseGroup4) {
+  const newExercise = await findOrCreateExercise(ExerciseGroup4Data);
+  G4exerciseIds.push(newExercise._id);
+}
+
+const KippingMuscleUp = await Workout.create({
+  // userId: Admin._id,
+  exerciseID: G4exerciseIds,
+  name: 'Muscle Up Start (Jumping)',
+  image: './UNIT5/week1/mon/GravityGains/backEnd/seed/Images/workoutImages/KippingMuscleUp.png',
+  lvl: 'Intermediate'
+});
+console.log(`Workout "${KippingMuscleUp.name}" created with exercises:`, G4exerciseIds);
+
+
+const ExerciseGroup5 = [
+  {
+    image: './GravityGains/backEnd/seed/Images/exerciseImages/MuscleUp.png',
+    name: 'Muscle Up',
+    lvl: 'Advanced',
+    video: 'https://youtu.be/YxBf7Vgh4Lc?si=LgTiUjNSVSZgH2Bf',
+    techniqueSummary: `
+      1. Pull behind bar
+      2. Keep elbows in
+      3. Rotate your wrists over the bar
+    `,
+    description: `
+      **Description:**
+      The Muscle-Up is an advanced calisthenic exercise that combines elements of pulling and pushing, requiring strength, explosiveness, and coordination. It targets almost all muscles in the upper body and is known for its three distinct phases: Pull, Transition, and Push.
+  
+      **Execution:**
+      1. **Pull:** Pull your body as high as possible, aiming to bring your chest or even hips to the level of the bar.
+      2. **Transition:** This is the challenging phase where you need to rotate your wrists over the bar and throw your shoulders over it.
+      3. **Push:** Complete the movement with a bar Dip, pushing yourself up to a fully extended position.
+  
+      **Muscle Engagement:**
+      The Muscle-Up engages multiple muscle groups in the upper body, requiring a combination of strength and coordination. It is an explosive movement that demands a seamless transition between pulling and pushing.
+  
+      **Variations:**
+      - The Muscle-Up can be performed on gymnastic rings, often using a false grip (start dead hang with wrists over rings).
+      - Experiment with different hand placements and grips to target specific muscle groups.
+    `,
+    categories: [bodyCategoriesArms._id, bodyCategoriesBack._id, bodyCategoriesChest._id]
+  },  
+  {
+    image: './GravityGains/backEnd/seed/Images/exerciseImages/KippingMuscleUp.png',
+    name: 'Kipping Muscle Up',
+    lvl: 'Advanced',
+    video: 'https://youtu.be/PMEhv0-WC34?si=6fC4_nb8dDKLefl7',
+    techniqueSummary: `
+      1. Swing forward
+      2. Use momentum
+      3. Rotate wrists
+      4. Shoulders over the bar
+      5. Elbows pullback
+    `,
+    description: `
+      **Description:**
+      The Kipping Muscle Up is an advanced calisthenic exercise that leverages controlled swinging and momentum to perform a dynamic transition over the bar. This technique-intensive movement engages various muscle groups and demands precise coordination for a fluid execution.
+  
+      **Execution:**
+      1. Initiate the movement by swinging forward to build up momentum.
+      2. As you swing back, use the accumulated momentum as extra assistance to pull yourself as high as possible.
+      3. Shortly before reaching the highest point, pull yourself towards the bar, focusing on pulling your elbows backward.
+      4. Actively rotate your wrists during the pull and throw your shoulders over the bar.
+      5. Once your chest is over the bar, execute a dip to extend your arms fully.
+      6. For the second repetition, use the momentum generated from the descent, similar to the initial swing forward.
+  
+      **Tips:**
+      - Master the timing of the swing and pull for efficient use of momentum.
+      - Practice controlled wrist rotation during the entire movement.
+      - Focus on maintaining a smooth and controlled descent to set up for subsequent repetitions.
+    `,
+    categories: [bodyCategoriesArms._id, bodyCategoriesBack._id, bodyCategoriesChest._id]
+  },
+  {
+    image: './UNIT5/week1/mon/GravityGains/backEnd/seed/Images/exerciseImages/PullUp.png',
+    name: 'Pull-Up',
+    lvl: 'Beginner',
+    video: 'https://www.youtube.com/watch?v=GBqAZP6jquc',
+    techniqueSummary: `
+      1. Scapula depressed and retracted
+      2. No swing
+      3. Full range of motion (ROM)
+    `,
+    description: `
+      **Description:**
+      The Pull-Up is a fundamental calisthenic exercise that serves as a prerequisite for mastering more advanced bar skills. The primary progression goal is to establish basic upper body strength, laying the foundation for achieving a Muscle Up.
+  
+      **Execution:**
+      1. Begin in a dead hang position.
+      2. Initiate the movement by depressing and retracting the scapula.
+      3. Engage your biceps and back muscles to pull yourself up, aiming for your chin to surpass the bar or ideally, your chest to touch the bar.
+      4. Descend slowly and with control back to the dead hang position.
+      5. It's crucial to perform the full range of motion for maximum effectiveness.
+  
+      **Variations:**
+      The Chin-Up, done with an underhand grip, differs from the overhand grip of the Pull-Up. The underhand grip causes your elbows to naturally bend more inward, targeting the biceps to a greater extent. The Pull-Up can also be performed on rings instead of bars.
+    `,
+    categories: [bodyCategoriesArms._id, bodyCategoriesBack._id]
+  },
+  {
+    image: './GravityGains/backEnd/seed/Images/exerciseImages/StraightBarDip.png',
+    name: 'Straight Bar Dip',
+    lvl: 'Beginner',
+    video: 'https://youtu.be/70y7r0vBddM?si=25ThDoaerUJ7jhwr',
+    techniqueSummary: `
+      1. Lower to about 90°
+      2. Keep scapula retracted on the way down
+      3. Keep elbows in
+    `,
+    description: `
+      **Description:**
+      The Straight Bar Dip is a beginner-level exercise that targets the muscles of the upper body, particularly the chest and triceps. It is an effective bodyweight movement for building strength and endurance in the arms and shoulders.
+  
+      **Execution:**
+      Start in the support position on a straight bar. Slowly lower yourself until your arms are at least in a 90° angle. On the way down, keep your scapula retracted, your elbows in, and your core tight. Push yourself up to the starting position.
+  
+      **Learn more:**
+      [Watch the video tutorial](https://youtu.be/70y7r0vBddM?si=25ThDoaerUJ7jhwr)
+    `,
+    categories: [bodyCategoriesArms._id, bodyCategoriesChest._id]
+  },
+];
+
+const G5exerciseIds = [];
+  
+for (const ExerciseGroup5Data of ExerciseGroup5) {
+  const newExercise = await findOrCreateExercise(ExerciseGroup5Data);
+  G5exerciseIds.push(newExercise._id);
+}
+
+const MuscleUp = await Workout.create({
+  // userId: Admin._id,
+  exerciseID: G5exerciseIds,
+  name: 'Muscle Up',
+  image: './UNIT5/week1/mon/GravityGains/backEnd/seed/Images/workoutImages/MuscleUp.png',
+  lvl: 'Advanced'
+});
+
+console.log(`Workout "${MuscleUp.name}" created with exercises:`, G5exerciseIds);
 
  ////Add new Skills\\\\
 // Create an array to store workout IDs
@@ -549,7 +804,7 @@ const MuscleUpSkill = await Skill.create({
   userId: Admin._id, // Associate it with Admin
   workoutID: MuscleUpWorkoutIds,
   name: 'Muscle Up',
-  // You might want to set an image for the skill as well
+  image: '',
 });
 
 console.log(`Skill "${MuscleUpSkill.name}" created with workouts:`, MuscleUpWorkoutIds);
